d5c81d6784e3d74044d31a8fab601fdf25cd5249
Danielle Madeley <danielle@madeley.id.au>
2009-12-19 16:37:52 +1100

    Implement keypad

--- ui/StopEntryDialog.py
+++ ui/StopEntryDialog.py
@@ -17,11 +17,23 @@ class StopEntryDialog(hildon.StackableWindow):
         contents = self.ui.get_object('stop-entry-dialog-contents')
         self.add(contents)
 
+        self.entry = self.ui.get_object('stopNo-entry')
+
         self.ui.connect_signals(self)
 
     def _entry_activate(self, entry):
         stopNo = entry.get_text()
         entry.set_text("")
         self.emit('stop-entered', stopNo)
+
+    def _keypad_button(self, button):
+        v = button.get_label()
+        self.entry.set_text(self.entry.get_text() + v)
+
+    def _keypad_backspace(self, button):
+        self.entry.set_text(self.entry.get_text()[:-1])
+
+    def _keypad_activate(self, button):
+        self._entry_activate(self.entry)
 
 gobject.type_register(StopEntryDialog)
